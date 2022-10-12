c415021429caa43146ba7968e08ab01a03f99217
Henric Zhang <henriczhang@berkeley.edu>
2022-10-11 18:40:11 -0700

    finish wk 2

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,16 +1,42 @@
 import { Button, Wrap, WrapItem, Text } from "@chakra-ui/react";
 import React from "react";
 
-const Footer = () => {
+const Footer = (Props) => {
+
+    const handleClick = () => {
+        Props.toEdit(1);
+    }
+
+    const getLastFive = (words) => {
+        var n = words.toString().split(" ");
+        var s = "";
+        console.log(words)
+        if (n.length < 5) {
+            s = words;
+            console.log(s)
+        } else {
+            for (var i = n.length - 5; i < n.length; i++) {
+                s += n[i];
+                s += " ";
+            }
+            s = s.slice(0, s.length - 1);
+        }
+        return s;
+    }
+
     return (
         <Wrap ml='4vw' justify='center'>
             <WrapItem>
-            <Button variant='Doc'>
+            <Button variant='Doc' onClick={handleClick}>
                 <img src='./add.svg'/>
             </Button>
             </WrapItem>
-            {Array.from(Array(11).keys()).map((obj, i) => {
-                return <WrapItem><Button variant='Reg'><Text variant='L4'>Lorem ipsum dolor sit amet, consectetur</Text></Button></WrapItem>
+            {Props.docs.map((obj, i) => {
+                console.log(obj)
+                return <WrapItem><Button onClick={() => {
+                    Props.change(i);
+                    Props.toEdit(1);
+                }} variant='Reg'><Text variant='L4'>{getLastFive(obj)}</Text></Button></WrapItem>
             })}
         </Wrap>
     )
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,14 +2,19 @@ import { Button } from '@chakra-ui/button';
 import React from 'react';
 import { Flex, Text } from '@chakra-ui/react';
 
-const Navbar = () => {
+const Navbar = (Props) => {
+
+  const handleClick = () => {
+    Props.toEdit(1);
+  }
+
   return (
     <Flex direction='row' justify='space-between' bg='cream'>
         <Flex direction='row' align='center' ml='3%' mt='3vh'>
             <img src={"./notiom.svg"}/>
             <Text variant='L3'>Notiom</Text>
         </Flex>
-        <Button variant="Create">Create</Button>
+        <Button variant="Create" onClick={handleClick}>Create</Button>
     </Flex>
   );
 }
